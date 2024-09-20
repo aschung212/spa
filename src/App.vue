e524cb8f65376e80560380b72a47970ccba91993
Aaron Chung <aschung212@gmail.com>
2024-09-20 12:52:40 -0700

    move bikes to header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Ducati logo" src="./assets/logo.png" />
+  <Header />
   <HelloWorld msg="Hello Aaron!" />
   <BasicDiv msg="Here's a basic div for you to play with" />
   <CustomComponent msg="!noraA olleH"/>
@@ -9,13 +9,15 @@
 import HelloWorld from './components/HelloWorld.vue'
 import CustomComponent from './components/CustomComponent.vue'
 import BasicDiv from './components/BasicDiv.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     CustomComponent,
-    BasicDiv
+    BasicDiv,
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="bikes">
+    <img alt="Ducati logo" src="../assets/logo.png" />
+    <img alt="Ducati logo" src="../assets/logo.png" />
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+    components: {
+    }
+}
+</script>
