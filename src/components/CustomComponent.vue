5b0b6dde83ba779949b7dcafaa579d27d24e7cf6
Aaron Chung <aschung212@gmail.com>
2024-09-20 14:28:13 -0700

    use script setup for CustomComponent and HelloWorld

--- src/components/CustomComponent.vue
+++ src/components/CustomComponent.vue
@@ -2,21 +2,19 @@
   <div :class="['countContainer', count%2 === 0 ? 'evenCount' : 'oddCount']">
     <h1>{{ msg }}</h1>
     <h2>count: {{ count }}</h2>
-    <button @click="count--" class="counterButton">-1</button>
+    <button @click="decreaseCount" class="counterButton">-1</button>
     <button @click="count = 0" type="button" class="resetButton">Reset</button>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'CustomComponent',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
-    }
-  }
+<script setup>
+import { ref } from 'vue'
+
+let count = ref(0);
+let decreaseCount = () => {
+  count.value--;
 }
+let props = defineProps({
+  msg: String
+})
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,22 +3,21 @@
     <h1>{{ msg }}</h1>
     <h2>count: {{ count }}</h2>
     <h1>{{ c }}</h1>
-    <button @click="count++" class="counterButton">+1</button>
+    <button @click="increaseCount" class="counterButton">+1</button>
     <button @click="count = 0" type="button" class="resetButton">Reset</button>
   </div>
 </template>
 
-<script>
-  export default {
-    name: 'HelloWorld',
-    props: {
-      msg: String,
-      c: Number
-    },
-    data() {
-      return {
-        count: 0
-      }
-    }
-  }
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0);
+
+const increaseCount = () => {
+  count.value++;
+}
+const props = defineProps({
+  msg: String,
+  c: Number
+});
 </script>
