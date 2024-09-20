3ac8ce8f50f1edfadb3cbb6cf6c7875ff0bf656e
Aaron Chung <aschung212@gmail.com>
2024-09-20 10:10:21 -0700

    add a custom component and change main logo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <img alt="Ducati logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Aaron!" />
+  <CustomComponent msg="custom message"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import CustomComponent from './components/CustomComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    CustomComponent
   }
 }
 </script>
---
+++ src/components/CustomComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count--">count is: {{ count }}</button>
+  <p>This is my custom component</p>
+</template>
+
+<script>
+export default {
+  name: 'CustomComponent',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
